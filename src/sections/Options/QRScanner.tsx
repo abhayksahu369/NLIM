0ac04223b70210ff966d9f8d02c1ab4fcaf8a10f
Abhay Kumar Sahu <abhayksahu.cs@gmail.com>
2025-03-15 16:53:45 +0530

    adsfasdfasdf

--- src/sections/Options/QRScanner.tsx
+++ src/sections/Options/QRScanner.tsx
@@ -26,7 +26,7 @@ const QRScanner: React.FC<QRScannerProps> = ({ onClose, onScanSuccess }) => {
         html5QrCode = new Html5Qrcode("reader");
         
         const qrboxFunction = (viewfinderWidth: number, viewfinderHeight: number) => {
-          const minEdgePercentage = window.innerWidth < 768 ? 0.9 : 0.7; // Larger QR box for mobile
+          const minEdgePercentage = window.innerWidth < 768 ? 1 : 0.7; // Larger QR box for mobile
           const minEdgeSize = Math.min(viewfinderWidth, viewfinderHeight);
           const qrboxSize = Math.max(Math.floor(minEdgeSize * minEdgePercentage), 200); // Ensure a minimum size
           return { width: qrboxSize, height: qrboxSize }; // Always a square
